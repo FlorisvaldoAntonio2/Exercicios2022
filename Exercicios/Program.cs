9528c23c2562fbd3d5cfd8ed19ba9e27930ffb46
flor <florisvaldoantonio2@gmail.com>
2022-02-18 12:28:41 -0300

    feito de 1 a 4

--- Exercicios/Program.cs
+++ Exercicios/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Exercicios.Class;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,17 @@ namespace Exercicios
     {
         static void Main(string[] args)
         {
+            //Exec.Questao01(10 , 0 , 0);
+
+            //Exec.Questao02(42);   
+
+            //Exec.Questao03(1010 , 1000 ,1010);
+
+            //Exec.Questao04(-11);
+
+            Exec.Questao05();
+
+            Console.ReadKey();
         }
     }
 }
